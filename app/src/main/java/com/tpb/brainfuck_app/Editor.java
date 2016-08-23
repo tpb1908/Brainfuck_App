8b21c6a0b74432546fa014843e9e3b7c05c1760f
tpb1908 <theo1908phone@gmail.com>
2016-08-23 15:47:52 +0100

    Set up the other buttons

--- app/src/main/java/com/tpb/brainfuck_app/Editor.java
+++ app/src/main/java/com/tpb/brainfuck_app/Editor.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -56,5 +57,64 @@ public class Editor extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    public void editButtonPress(View v) {
+        final int start = Math.max(mEditor.getSelectionStart(), 0);
+        final int end = Math.max(mEditor.getSelectionEnd(), 0);
+        switch(v.getId()) {
+            //TODO- Replace the text replacement with key events?
+            case R.id.decrement_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), "<");
+                break;
+            case R.id.increment_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), ">");
+                break;
+            case R.id.plus_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), "+");
+                break;
+            case R.id.minus_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), "-");
+                break;
+            case R.id.output_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), ".");
+                break;
+            case R.id.input_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), ",");
+                break;
+            case R.id.start_loop_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), "[");
+                break;
+            case R.id.end_loop_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), "]");
+                break;
+            case R.id.space_button:
+                mEditor.getText().replace(Math.min(start, end), Math.max(start, end), " ");
+                break;
+            case R.id.backspace_button:
+                mEditor.dispatchKeyEvent(new KeyEvent(0,0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL, 0));
+                mEditor.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DEL, 0));
+                break;
+            case R.id.backward_button:
+                mEditor.dispatchKeyEvent(new KeyEvent(0,0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_LEFT, 0));
+                mEditor.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DPAD_LEFT, 0));
+                break;
+            case R.id.forward_button:
+                mEditor.dispatchKeyEvent(new KeyEvent(0,0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_RIGHT, 0));
+                mEditor.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DPAD_RIGHT, 0));
+                break;
+            case R.id.up_button:
+                mEditor.dispatchKeyEvent(new KeyEvent(0,0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_UP, 0));
+                mEditor.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DPAD_UP, 0));
+                break;
+            case R.id.down_button:
+                mEditor.dispatchKeyEvent(new KeyEvent(0,0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DPAD_DOWN, 0));
+                mEditor.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_DPAD_DOWN, 0));
+                break;
+            case R.id.enter_button:
+                mEditor.dispatchKeyEvent(new KeyEvent(0,0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER, 0));
+                mEditor.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_UP, KeyEvent.KEYCODE_ENTER, 0));
+                break;
+        }
     }
 }
