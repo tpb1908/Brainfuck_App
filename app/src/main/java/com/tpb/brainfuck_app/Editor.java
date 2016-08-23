bc6b96e4c23967568ad28ca4dcb58289f32b6763
tpb1908 <theo1908phone@gmail.com>
2016-08-23 15:22:48 +0100

    Set up keyboard lock button

--- app/src/main/java/com/tpb/brainfuck_app/Editor.java
+++ app/src/main/java/com/tpb/brainfuck_app/Editor.java
@@ -8,13 +8,17 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * Created by theo on 21/08/16.
  */
 public class Editor extends AppCompatActivity {
-    private boolean isKeyboardLocked;
+    private boolean isKeyboardLocked = false;
     private EditText mEditor;
+    private ImageButton mKeyboardLock;
+    private ImageButton mRun;
+    private ImageButton mQuickRun;
 
 
     @Override
@@ -22,15 +26,34 @@ public class Editor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editor);
         mEditor = (EditText) findViewById(R.id.editor);
+        mKeyboardLock = (ImageButton) findViewById(R.id.lock_keyboard_button);
+        mRun = (ImageButton) findViewById(R.id.run_button);
+        mQuickRun = (ImageButton) findViewById(R.id.quick_run_button);
 
 
         mEditor.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 view.onTouchEvent(motionEvent);
-                final InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-                if(imm != null) imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                if(isKeyboardLocked) {
+                    final InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if(imm != null) imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
                 return true;
+            }
+        });
+
+        mKeyboardLock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isKeyboardLocked = ! isKeyboardLocked;
+                if(isKeyboardLocked) {
+                    mKeyboardLock.setImageDrawable(getApplicationContext().getDrawable(R.drawable.ic_lock_outline_black));
+                    final InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if(imm != null) imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                } else {
+                    mKeyboardLock.setImageDrawable(getApplicationContext().getDrawable(R.drawable.ic_lock_open_black));
+                }
             }
         });
     }
